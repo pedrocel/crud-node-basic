c1699112770ec5974cb16563faf44ddfd9b0460a
pedrocel <pedrinhonovais10@gmail.com>
2023-03-13 17:52:24 -0300

    feat: include sql file

---
+++ .sql
@@ -0,0 +1,8 @@
+CREATE DATABASE graduations;
+
+CREATE TABLE graduations (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    created_at VARCHAR(45) NOT NULL
+)
